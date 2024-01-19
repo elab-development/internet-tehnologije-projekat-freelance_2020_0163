578bff7f738c1c397626597b3ac3439941f9cdbc
Vasilije Milacic <milacicvasilije@gmail.com>
2024-01-19 19:08:43 +0100

    Uradjena footer komponenta i njen css

---
+++ freelance-frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Footer.css'; 
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <p> 2024 IT Freelance App</p>
+        <p>Kontakt: freelance@gmail.com</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
